b2bbe4f3a3a827edf91eb2c1f77fa540de5570f0
Malak Sobhy <167289328+Malkah04@users.noreply.github.com>
2025-12-13 10:06:28 +0200

    Refactor onCreate to use AppNavGraph
    Removed commented-out Scaffold code and added AppNavGraph.

--- app/src/main/java/com/example/finalproject_tazkartm3aj/MainActivity.kt
+++ app/src/main/java/com/example/finalproject_tazkartm3aj/MainActivity.kt
@@ -15,18 +15,13 @@ import androidx.navigation.compose.rememberNavController
 import com.example.finalproject_tazkartm3aj.ui.theme.FinalProjecttazkartM3ajTheme
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+   override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             FinalProjecttazkartM3ajTheme {
-//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-//                    Greeting(
-//                        name = "",
-//                        modifier = Modifier.padding(innerPadding)
-//                    )
-//
-//                }
+                val navController = rememberNavController()
+                AppNavGraph(navController)
             }
         }
     }
